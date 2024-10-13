f7ce6f6a93ae5c939121d6f2356b74d1ea8378d3
Mikhail <Mikhail1Solovev@gmail.com>
2024-10-13 20:41:25 +0500

    Bugs fixed

--- habit_tracker/celery.py
+++ habit_tracker/celery.py
@@ -1,6 +1,7 @@
 import os
 from celery import Celery
 from django.conf import settings
+from datetime import timedelta
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'habit_tracker.settings')
 
@@ -9,6 +10,14 @@ app = Celery('habit_tracker')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+CELERY_BEAT_SCHEDULE = {
+    'send_reminder': {
+        'task': 'telegram_bot.tasks.send_reminder',
+        'schedule': timedelta(hours=1),  # Настроить по необходимости
+        'args': ['<CHAT_ID>', 'Напоминание о привычке']
+    },
+}
 
 
 @app.task(bind=True)
--- users/views.py
+++ users/views.py
@@ -1,12 +1,18 @@
 from rest_framework import viewsets, permissions
 from .models import CustomUser
 from .serializers import UserSerializer
+from rest_framework.permissions import AllowAny
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+
+    # Разрешить неавторизованным пользователям доступ к регистрации
+    def get_permissions(self):
+        if self.action == 'create':
+            return [AllowAny()]
+        return [permissions.IsAuthenticated()]
 
     def get_queryset(self):
-        return self.queryset.filter(is_active=True)
+        return self.queryset.filter(is_active=Tru
